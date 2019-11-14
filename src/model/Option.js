fe494fc7ce811a1b7765aac04e29506b4e55019f
Jeff <sagalbot@gmail.com>
2019-11-13 18:01:23 -0800

    WIP - started a class, but got inspired to try something in the typescript branch

---
+++ src/model/Option.js
@@ -0,0 +1,15 @@
+export default class Option {
+
+  #option;
+  #VueSelect;
+
+  constructor (option, VueSelect) {
+    this.option = option;
+    this.VueSelect = VueSelect;
+  }
+
+  label () {
+
+  }
+
+}
